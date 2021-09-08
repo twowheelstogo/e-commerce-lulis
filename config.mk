2d2312cab2d39ffe925e8e271e5dd2a319f6ab92
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-09-08 16:33:56 -0600

    remove invoice repo

--- config.mk
+++ config.mk
@@ -31,8 +31,7 @@ https://github.com/twowheelstogo/reaction-hydra-lulis.git,reaction-hydra,trunk \
 https://github.com/twowheelstogo/reaction-lulis.git,reaction,trunk \
 https://github.com/twowheelstogo/reaction-identity-lulis.git,reaction-identity,trunk \
 https://github.com/twowheelstogo/reaction-admin-lulis.git,reaction-admin,f_odoo \
-https://github.com/twowheelstogo/storefront-lulis.git,example-storefront,trunk \
-https://github.com/twowheelstogo/backend-invoice-module.git,backend-invoice-module,main
+https://github.com/twowheelstogo/storefront-lulis.git,example-storefront,trunk 
 endef
 
 # These are all the plugins that `make clone-api-plugins` will clone.
